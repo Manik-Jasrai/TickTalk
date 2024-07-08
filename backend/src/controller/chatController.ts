660daf16bd9f8bef004b4a2a97cc9b7bfe6a3a26
Manik-Jasrai <jasraimanik@gmail.com>
2024-07-08 19:18:11 +0530

    chatController clean up

--- backend/src/controller/chatController.ts
+++ backend/src/controller/chatController.ts
@@ -1,17 +1,22 @@
 import { Request, Response } from "express";
-import Chat from "../model/Chat";
 import User from "../model/User";
-import { PopulateOptions } from "mongoose";
 
 export const getAllChats = async (req : Request,res : Response) => {
     // get all chats of logged in user
     const user = req.user;
     if (!user) return res.sendStatus(401);
 
-    const validUser = await User.findOne({username : user.username}).populate({path : "chats" , populate : {path : "members messages lastMessage"}}).exec();
-    if (!validUser) return res.status(401).json({'message' : 'User not found'});
-    
+    const validUser = await User.findOne({ username: user.username })
+            .populate({
+                path: "chats",
+                populate: [
+                    { path: "members" },
+                    { path: "messages" },
+                    { path: "lastMessage", select: "timeStamp" }
+                ]
+            })
+            .exec();
+    if (!validUser) return res.status(401).json({'message' : 'User not found'});   
     const chats = validUser.chats;
-
     res.json({'chats' : chats});
 };
